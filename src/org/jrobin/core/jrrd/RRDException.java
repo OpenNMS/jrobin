ae2fc245e8343a5e2b63851d01bc319e2ff64345
DJ Gregor <dj@opennms.org>
2005-09-06 21:27:04 +0000

    fix import conflicts, take two; anyone want to take bets on whether or not I'm done?

---
+++ src/org/jrobin/core/jrrd/RRDException.java
@@ -0,0 +1,33 @@
+/*
+ * Copyright (C) 2001 Ciaran Treanor <ciaran@codeloop.com>
+ *
+ * Distributable under GPL license.
+ * See terms of license at gnu.org.
+ *
+ * $Id$
+ */
+package org.jrobin.core.jrrd;
+
+/**
+ * This exception may be throw if an error occurs while operating
+ * on an RRD object.
+ *
+ * @author <a href="mailto:ciaran@codeloop.com">Ciaran Treanor</a>
+ * @version $Revision$
+ */
+public class RRDException extends Exception {
+
+	/**
+	 * Constructs an RRDException with no detail message.
+	 */
+	public RRDException() {
+		super();
+	}
+
+	/**
+	 * Constructs an RRDException with the specified detail message.
+	 */
+	public RRDException(String message) {
+		super(message);
+	}
+}
