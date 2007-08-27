50743da9579dbbd32b4c8c8037ddc1be9c879e19
Benjamin Reed <rangerrick@opennms.org>
2007-08-27 19:53:54 +0000

    clean up the whitespace

--- src/org/jrobin/graph/CommentText.java
+++ src/org/jrobin/graph/CommentText.java
@@ -28,45 +28,44 @@ import org.jrobin.core.RrdException;
 import org.jrobin.data.DataProcessor;
 
 class CommentText implements RrdGraphConstants {
-	private final String text;		// original text
+    private final String text; // original text
 
-	String resolvedText;	// resolved text
-	String marker; // end-of-text marker
-	boolean enabled; // hrule and vrule comments can be disabled at runtime
-	int x, y; // coordinates, evaluated later
+    String resolvedText; // resolved text
+    String marker; // end-of-text marker
+    boolean enabled; // hrule and vrule comments can be disabled at runtime
+    int x, y; // coordinates, evaluated later
 
-	CommentText(String text) {
-		this.text = text;
-	}
+    CommentText(String text) {
+        this.text = text;
+    }
 
-	void resolveText(DataProcessor dproc, ValueScaler valueScaler) throws RrdException {
-		resolvedText = text;
-		marker = "";
-		if (resolvedText != null) {
-			for (String mark : MARKERS) {
-				if (resolvedText.endsWith(mark)) {
-					marker = mark;
-					resolvedText = resolvedText.substring(0, resolvedText.length() - marker.length());
-					trimIfGlue();
-					break;
-				}
-			}
-		}
-		enabled = resolvedText != null;
-	}
+    void resolveText(DataProcessor dproc, ValueScaler valueScaler) throws RrdException {
+        resolvedText = text;
+        marker = "";
+        if (resolvedText != null) {
+            for (String mark : MARKERS) {
+                if (resolvedText.endsWith(mark)) {
+                    marker = mark;
+                    resolvedText = resolvedText.substring(0, resolvedText.length() - marker.length());
+                    trimIfGlue();
+                    break;
+                }
+            }
+        }
+        enabled = resolvedText != null;
+    }
 
+    void trimIfGlue() {
+        if (marker.equals(GLUE_MARKER)) {
+            resolvedText = resolvedText.trim();
+        }
+    }
 
-	void trimIfGlue() {
-		if (marker.equals(GLUE_MARKER)) {
-			resolvedText = resolvedText.trim();
-		}
-	}
+    boolean isPrint() {
+        return false;
+    }
 
-	boolean isPrint() {
-		return false;
-	}
-
-	boolean isValidGraphElement() {
-		return !isPrint() && enabled;
-	}
+    boolean isValidGraphElement() {
+        return !isPrint() && enabled;
+    }
 }
