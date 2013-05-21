c6b9cd3736eab403050526019b397e598681cf22
Benjamin Reed <rangerrick@befunk.com>
2013-05-21 18:19:04 -0400

    fix up test to test NIO issue

--- test/org/jrobin/core/JRobinTest.java
+++ test/org/jrobin/core/JRobinTest.java
@@ -18,21 +18,18 @@
  *******************************************************************************/
 package org.jrobin.core;
 
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+
 import java.util.Random;
 
-import static org.junit.Assert.*;
-
-import org.jrobin.core.RrdBackendFactory;
-import org.jrobin.core.RrdDb;
-import org.jrobin.core.RrdDef;
-import org.jrobin.core.RrdNioBackendFactory;
-import org.jrobin.core.Sample;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
 public class JRobinTest {
+    RrdDef m_def = null;
     RrdDb m_db = null;
 
     Random m_random = new Random();
@@ -40,31 +37,26 @@ public class JRobinTest {
     @BeforeClass
     public static void beforeClass() throws Exception {
         if (!RrdBackendFactory.isInstanceCreated()) {
-            RrdBackendFactory.setDefaultFactory("NIO");
+            RrdBackendFactory.setDefaultFactory("FILE");
         }
     }
 
     @Before
     public void setUp() throws Exception {
-        RrdDef def = new RrdDef("target/test.jrb");
-        def.setStep(300);
-        def.addDatasource("test", "GAUGE", 300L, Double.NaN, Double.NaN);
-        def.addArchive("RRA:AVERAGE:0.5:1:2016");
-        m_db = new RrdDb(def);
+        m_def = new RrdDef("target/test.jrb");
+        m_def.setStep(300);
+        m_def.addDatasource("test", "GAUGE", 300L, Double.NaN, Double.NaN);
+        m_def.addArchive("RRA:AVERAGE:0.5:1:2016");
     }
 
     @After
     public void tearDown() throws Exception {
-        RrdNioBackendFactory factory = (RrdNioBackendFactory) RrdBackendFactory.getDefaultFactory();
-        final SyncManager syncManager = factory.getSyncManager();
-        assertNotNull(syncManager);
-        assertNotNull(syncManager.getTimer());
-        m_db.close();
-        assertNull(syncManager.getTimer());
+        if (m_db != null) m_db.close();
     }
 
     @Test
     public void testRrdUpdate() throws Exception {
+        m_db = new RrdDb(m_def);
         Sample sample = null;
         long startTime = System.currentTimeMillis() - 1000;
         // for (int i = 0; i < 1000000; i++) {
@@ -72,5 +64,22 @@ public class JRobinTest {
         sample.setAndUpdate(startTime + ":" + m_random.nextInt());
         // sample.setAndUpdate(startTime++ + ":" + m_random.nextInt());
         // }
+    }
+    
+    @Test
+    public void testRrdNioBackend() throws Exception {
+        final RrdNioBackendFactory factory = (RrdNioBackendFactory)RrdBackendFactory.getFactory("NIO");
+        m_db = new RrdDb(m_def, factory);
+        Sample sample = null;
+        long startTime = System.currentTimeMillis() - 1000;
+        sample = m_db.createSample();
+        sample.setAndUpdate(startTime + ":" + m_random.nextInt());
+
+        final SyncManager syncManager = factory.getSyncManager();
+        assertNotNull(syncManager);
+        assertNotNull(syncManager.getTimer());
+        m_db.close();
+        m_db = null;
+        assertNull(syncManager.getTimer());
     }
 }
